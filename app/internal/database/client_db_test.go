f29eb044ed478391b0885fdab76caf7469df5714
leoCardosoDev <leocardosodev@gmail.com>
2023-11-26 08:06:43 -0300

    refactor: rename db memory 2

--- app/internal/database/client_db_test.go
+++ app/internal/database/client_db_test.go
@@ -16,7 +16,7 @@ type ClientDBTestSuite struct {
 }
 
 func (s *ClientDBTestSuite) SetupSuite() {
-	db, err := sql.Open("sqlite3", ":memory")
+	db, err := sql.Open("sqlite3", ":memory:")
 	s.Nil(err)
 	s.db = db
 	db.Exec("CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), created_at date)")
