85150531cb182b868adab6aa74f8bc6c081ac75d
leoCardosoDev <leocardosodev@gmail.com>
2023-12-04 05:35:36 -0300

    feat: add webserver to main go

---
+++ app/cmd/walletcore/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/database"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/event"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/usecase/create_account"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/usecase/create_client"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/usecase/create_transaction"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/web"
+	"github.com.br/devfullcycle/fc-ms-wallet/internal/web/webserver"
+	"github.com.br/devfullcycle/fc-ms-wallet/pkg/events"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", "root", "root", "mysql", "3306", "wallet"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	eventDispatcher := events.NewEventDispatcher()
+	transactionCreatedEvent := event.NewCreateTransactionCreated()
+	// eventDispatcher.Register("TransactionCreated", handler)
+	clientDb := database.NewClientDb(db)
+	accountDb := database.NewAccountDB(db)
+	transactionDb := database.NewTransactionDB(db)
+
+	createClientUsecase := create_client.NewCreateClientUseCase(clientDb)
+	createAccountUsecase := create_account.NewCreateAccountUseCase(accountDb, clientDb)
+	createTransactionUsecase := create_transaction.NewCreateTransactionUseCase(transactionDb, accountDb, eventDispatcher, transactionCreatedEvent)
+
+	webserver := webserver.NewWebServer(":3000")
+	
+	clientHandler := web.NewWebClientHandler(*createClientUsecase)
+	accountHandler := web.NewWebAccountHandler(*createAccountUsecase)
+	transactionHandler := web.NewWebTransactionHandler(*createTransactionUsecase)
+
+	webserver.AddHandler("/clients", clientHandler.CreateClient)
+	webserver.AddHandler("/accounts", accountHandler.CreateAccount)
+	webserver.AddHandler("/transactions", transactionHandler.CreateTransaction)
+
+	webserver.Start()
+}
