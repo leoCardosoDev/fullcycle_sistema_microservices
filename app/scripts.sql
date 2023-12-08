476fb13f200b85671de787aa38adb51d2ee90257
leoCardosoDev <leocardosodev@gmail.com>
2023-12-08 08:17:07 -0300

    refactor: adjust sql scripts

--- app/scripts.sql
+++ app/scripts.sql
@@ -2,4 +2,4 @@ CREATE TABLE clients (id varchar(255), name varchar(255), email varchar(255), cr
 CREATE TABLE accounts (id varchar(255), client_id varchar(255), balance float, created_at date);
 CREATE TABLE transactions (id varchar(255), account_id_from varchar(255), account_id_to varchar(255), amount float, created_at date);
 
-UPDATE accounts SET balance = 1000 where id = "7372c79a-e1c5-467f-9178-b9b4072bf901";
+UPDATE accounts SET balance = 1000 where id = "84b29a03-d5a6-429a-b466-36f82db8e427";
